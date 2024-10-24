e5b3ef6e287cf9f469e01ea1e927d2adc53b3f83
Over1Cloud <blackcrbear@gmail.com>
2024-10-25 01:08:14 +0300

    Your commit message

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     YANDEX_CLIENT_ID = '686d26571d284e249606165535c71c89'
     YANDEX_CLIENT_SECRET = 'd4cb9804ab0c4ec6a705ddcfcaa0e513'
-    YANDEX_REDIRECT_URI = 'http://localhost:8000/callback'
+    YANDEX_REDIRECT_URI = 'http://193.233.164.46:8000/callback'
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'static', 'uploads')
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # Устанавливаем максимальный размер файла в 100 МБ
     ALLOWED_EXTENSIONS = set()  # Пустое множество, разрешаем все типы файлов
