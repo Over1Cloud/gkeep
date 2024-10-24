0b23d151665dcef109fafc4ad2e32ed10294d795
Over1Cloud <blackcrbear@gmail.com>
2024-10-25 00:08:44 +0300

    Your commit message

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app.models import User, Note, ListItem, Reminder
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создает таблицы заново
-    app.run(debug=True)
+    app.run(debug=True, port=8000)  # Запуск на порту 8000
